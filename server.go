0114a8d17cf441ae107f8bb3cdcd4a056086d421
aasumitro <aasumitro>
2019-02-02 21:33:42 +0800

    structuring folder

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+	return r
+}
+
+func main() {
+	// The router is now formed by calling the `newRouter` constructor function
+	// that we defined above. The rest of the code stays the same
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
